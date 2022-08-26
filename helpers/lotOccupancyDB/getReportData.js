679e1e7a55fe6acd1b7ce61b6ddabb74eb7fb219
Dan Gowans <d.gowans@cityssm.on.ca>
2022-08-26 15:05:52 -0400

    table export reports

--- helpers/lotOccupancyDB/getReportData.js
+++ helpers/lotOccupancyDB/getReportData.js
@@ -35,6 +35,66 @@ export const getReportData = (reportName, reportParameters) => {
         case "lots-all":
             sql = "select * from Lots";
             break;
+        case "lotComments-all":
+            sql = "select * from LotComments";
+            break;
+        case "lotFields-all":
+            sql = "select * from LotFields";
+            break;
+        case "lotOccupancies-all":
+            sql = "select * from LotOccupancies";
+            break;
+        case "lotOccupancyComments-all":
+            sql = "select * from LotOccupancyComments";
+            break;
+        case "lotOccupancyFees-all":
+            sql = "select * from LotOccupancyFees";
+            break;
+        case "lotOccupancyFields-all":
+            sql = "select * from LotOccupancyFields";
+            break;
+        case "lotOccupancyOccupants-all":
+            sql = "select * from LotOccupancyOccupants";
+            break;
+        case "lotOccupancyTransactions-all":
+            sql = "select * from LotOccupancyTransactions";
+            break;
+        case "workOrders-all":
+            sql = "select * from WorkOrders";
+            break;
+        case "workOrderComments-all":
+            sql = "select * from WorkOrderComments";
+            break;
+        case "workOrderLots-all":
+            sql = "select * from WorkOrderLots";
+            break;
+        case "fees-all":
+            sql = "select * from Fees";
+            break;
+        case "feeCategories-all":
+            sql = "select * from FeeCategories";
+            break;
+        case "lotTypes-all":
+            sql = "select * from LotTypes";
+            break;
+        case "lotTypeFields-all":
+            sql = "select * from LotTypeFields";
+            break;
+        case "lotStatuses-all":
+            sql = "select * from LotStatuses";
+            break;
+        case "occupancyTypes-all":
+            sql = "select * from OccupancyTypes";
+            break;
+        case "occupancyTypeFields-all":
+            sql = "select * from OccupancyTypeFields";
+            break;
+        case "lotOccupantTypes-all":
+            sql = "select * from LotOccupantTypes";
+            break;
+        case "workOrderTypes-all":
+            sql = "select * from WorkOrderTypes";
+            break;
         default:
             return undefined;
     }
