4ab29dd5dfc4c2fee37df89fe21b11ed1f05ad4a
Dan Gowans <d.gowans@cityssm.on.ca>
2022-08-30 14:47:31 -0400

    linting

--- handlers/maps-post/doCreateMap.ts
+++ handlers/maps-post/doCreateMap.ts
@@ -1,16 +1,20 @@
-import type { RequestHandler } from "express";
+import type {
+    RequestHandler
+} from "express";
 
-import { addMap } from "../../helpers/lotOccupancyDB/addMap.js";
+import {
+    addMap
+} from "../../helpers/lotOccupancyDB/addMap.js";
 
 
 export const handler: RequestHandler = async (request, response) => {
 
-  const mapId = addMap(request.body, request.session);
+    const mapId = addMap(request.body, request.session);
 
-  response.json({
-    success: true,
-    mapId
-  });
+    response.json({
+        success: true,
+        mapId
+    });
 };
 
 
--- handlers/maps-post/doUpdateMap.ts
+++ handlers/maps-post/doUpdateMap.ts
@@ -1,16 +1,20 @@
-import type { RequestHandler } from "express";
+import type {
+    RequestHandler
+} from "express";
 
-import { updateMap } from "../../helpers/lotOccupancyDB/updateMap.js";
+import {
+    updateMap
+} from "../../helpers/lotOccupancyDB/updateMap.js";
 
 
 export const handler: RequestHandler = async (request, response) => {
 
-  const success = updateMap(request.body, request.session);
+    const success = updateMap(request.body, request.session);
 
-  response.json({
-    success,
-    mapId: request.body.mapId
-  });
+    response.json({
+        success,
+        mapId: request.body.mapId
+    });
 };
 
 
