4c202e69f1036bbb9ffe96c0d42d41ce14883da4
Dan Gowans <d.gowans@cityssm.on.ca>
2022-09-09 13:35:38 -0400

    change sorting of milestones

--- helpers/lotOccupancyDB/getWorkOrderMilestones.js
+++ helpers/lotOccupancyDB/getWorkOrderMilestones.js
@@ -14,12 +14,17 @@ export const getWorkOrderMilestones = (workOrderId, connectedDatabase) => {
         " m.workOrderMilestoneDate, userFn_dateIntegerToString(m.workOrderMilestoneDate) as workOrderMilestoneDateString," +
         " m.workOrderMilestoneTime, userFn_timeIntegerToString(m.workOrderMilestoneTime) as workOrderMilestoneTimeString," +
         " m.workOrderMilestoneDescription," +
+        " m.workOrderMilestoneCompletionDate, userFn_dateIntegerToString(m.workOrderMilestoneCompletionDate) as workOrderMilestoneCompletionDateString," +
+        " m.workOrderMilestoneCompletionTime, userFn_timeIntegerToString(m.workOrderMilestoneCompletionTime) as workOrderMilestoneCompletionTimeString," +
         " m.recordCreate_userName, m.recordUpdate_userName" +
         " from WorkOrderMilestones m" +
         " left join WorkOrderMilestoneTypes t on m.workOrderMilestoneTypeId = t.workOrderMilestoneTypeId" +
         " where m.recordDelete_timeMillis is null" +
         " and m.workOrderId = ?" +
-        " order by workOrderMilestoneDate desc, workOrderMilestoneTime desc, workOrderMilestoneId desc")
+        " order by" +
+        " m.workOrderMilestoneDate, m.workOrderMilestoneTime," +
+        " m.workOrderMilestoneCompletionDate, m.workOrderMilestoneCompletionTime," +
+        " t.orderNumber, m.workOrderMilestoneId")
         .all(workOrderId);
     if (!connectedDatabase) {
         database.close();
