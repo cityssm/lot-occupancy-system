b0a6e1b0851e4c57e935b79aec1e7269a5d73d16
Dan Gowans <d.gowans@cityssm.on.ca>
2022-09-20 15:13:34 -0400

    initial testing

---
+++ cypress/e2e/01-admin/cleanupDatabase.cy.ts
@@ -0,0 +1,38 @@
+/* eslint-disable unicorn/filename-case, promise/catch-or-return, promise/always-return */
+
+import { testAdmin } from "../../../test/_globals.js";
+
+import { logout, login, ajaxDelayMillis } from "../../support/index.js";
+
+describe("Admin - Cleanup Database", () => {
+    before(() => {
+        logout();
+        login(testAdmin);
+    });
+
+    after(logout);
+
+    beforeEach("Loads page", () => {
+        cy.visit("/admin/cleanup");
+        cy.location("pathname").should("equal", "/admin/cleanup");
+    });
+
+    it("Has no detectable accessibility issues", () => {
+        cy.injectAxe();
+        cy.checkA11y();
+    });
+
+    it("Cleans up the database", () => {
+        cy.get("button[data-cy='cleanup']").click();
+
+        cy.get(".modal").should("be.visible").should("contain.text", "Cleanup");
+
+        cy.get(".modal button[data-cy='ok']").click();
+
+        cy.wait(ajaxDelayMillis);
+
+        cy.get(".modal").should("contain.text", "Cleaned Up").should("contain.text", "Success");
+
+        cy.get(".modal button[data-cy='ok']").click();
+    });
+});
