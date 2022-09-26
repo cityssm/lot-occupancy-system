d5106b9784673168de37a6252675b7da6d601a4f
Dan Gowans <d.gowans@cityssm.on.ca>
2022-09-26 15:15:27 -0400

    use a side menu for reports

---
+++ public-typescript/reportSearch.js
@@ -0,0 +1,24 @@
+(() => {
+    const menuTabElements = document.querySelectorAll(".menu a");
+    const tabContainerElements = document.querySelectorAll(".tabs-container > div");
+    const selectTab = (clickEvent) => {
+        clickEvent.preventDefault();
+        for (const menuTabElement of menuTabElements) {
+            menuTabElement.classList.remove("is-active");
+        }
+        const selectedTabElement = clickEvent.currentTarget;
+        selectedTabElement.classList.add("is-active");
+        const selectedTabContainerId = selectedTabElement.href.slice(Math.max(0, selectedTabElement.href.indexOf("#") + 1));
+        for (const tabContainerElement of tabContainerElements) {
+            if (tabContainerElement.id === selectedTabContainerId) {
+                tabContainerElement.classList.remove("is-hidden");
+            }
+            else {
+                tabContainerElement.classList.add("is-hidden");
+            }
+        }
+    };
+    for (const menuTabElement of menuTabElements) {
+        menuTabElement.addEventListener("click", selectTab);
+    }
+})();
---
+++ public-typescript/reportSearch.ts
@@ -0,0 +1,38 @@
+(() => {
+
+    const menuTabElements = document.querySelectorAll(".menu a") as NodeListOf<HTMLAnchorElement>;
+    const tabContainerElements = document.querySelectorAll(".tabs-container > div") as NodeListOf<HTMLElement>;
+
+    const selectTab = (clickEvent: Event) => {
+        clickEvent.preventDefault();
+
+        // Remove .is-active from all tabs
+
+        for (const menuTabElement of menuTabElements) {
+            menuTabElement.classList.remove("is-active");
+        }
+
+        // Set .is-active on clicked tab
+
+        const selectedTabElement = clickEvent.currentTarget as HTMLAnchorElement;
+        selectedTabElement.classList.add("is-active");
+
+        // Hide all but selected tab
+
+        const selectedTabContainerId = selectedTabElement.href.slice(Math.max(0, selectedTabElement.href.indexOf("#") + 1));
+
+        for (const tabContainerElement of tabContainerElements) {
+
+            if (tabContainerElement.id === selectedTabContainerId) {
+                tabContainerElement.classList.remove("is-hidden");
+            } else {
+                tabContainerElement.classList.add("is-hidden");
+            }
+        }
+    };
+
+
+    for (const menuTabElement of menuTabElements) {
+        menuTabElement.addEventListener("click", selectTab);
+    }
+})();
