a4b857a869bb961e1207257a0ffa0a24ce456b9d
Dan Gowans <d.gowans@cityssm.on.ca>
2022-10-04 12:09:19 -0400

    work order number colour

---
+++ public-typescript/dashboard.ts
@@ -0,0 +1,13 @@
+/* eslint-disable unicorn/prefer-module */
+
+import type * as globalTypes from "../types/globalTypes";
+
+(() => {
+    const los = exports.los as globalTypes.LOS;
+
+    const workOrderNumberCircleElements = document.querySelectorAll(".fa-circle[data-work-order-number") as NodeListOf<HTMLElement>;
+
+    for (const workOrderNumberCircleElement of workOrderNumberCircleElements) {
+        workOrderNumberCircleElement.style.color = los.getRandomColor(workOrderNumberCircleElement.dataset.workOrderNumber);
+    }
+})();
