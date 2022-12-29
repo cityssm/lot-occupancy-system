0e64daaf5357422c8446206ff81ba1b8a1adcf67
Dan Gowans <d.gowans@cityssm.on.ca>
2022-12-29 16:24:21 -0500

    fix expansion mistake

--- handlers/admin-post/doAddFee.js
+++ handlers/admin-post/doAddFee.js
@@ -1,7 +1,7 @@
 import { addFee } from "../../helpers/lotOccupancyDB/addFee.js";
 import { getFeeCategories } from "../../helpers/lotOccupancyDB/getFeeCategories.js";
 export const handler = async (request, response) => {
-    const feeId = addFee({ feeForm: request.body, requestSession: request.session });
+    const feeId = addFee(request.body, request.session);
     const feeCategories = getFeeCategories({}, {
         includeFees: true
     });
