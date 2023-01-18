82c7f047cfcc85e54b852f0cb5d8de19087bb530
Dan Gowans <d.gowans@cityssm.on.ca>
2023-01-18 14:00:17 -0500

    close pool connections

--- helpers/lotOccupancyDB/pool.js
+++ helpers/lotOccupancyDB/pool.js
@@ -1,6 +1,10 @@
 import { Pool } from 'better-sqlite-pool';
 import { lotOccupancyDB as databasePath } from '../../data/databasePaths.js';
+import exitHook from 'exit-hook';
 const pool = new Pool(databasePath);
 export async function acquireConnection() {
     return await pool.acquire();
 }
+exitHook(() => {
+    pool.close();
+});
--- helpers/lotOccupancyDB/pool.ts
+++ helpers/lotOccupancyDB/pool.ts
@@ -2,8 +2,14 @@ import { Pool, PoolConnection } from 'better-sqlite-pool'
 
 import { lotOccupancyDB as databasePath } from '../../data/databasePaths.js'
 
+import exitHook from 'exit-hook'
+
 const pool = new Pool(databasePath)
 
 export async function acquireConnection(): Promise<PoolConnection> {
   return await pool.acquire()
 }
+
+exitHook(() => {
+  pool.close()
+})
