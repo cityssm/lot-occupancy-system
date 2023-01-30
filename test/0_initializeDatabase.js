8d4bdaa37182da68954ed823ce8203fc84d1c6ef
Dan Gowans <d.gowans@cityssm.on.ca>
2023-01-30 15:03:59 -0500

    fix database create

--- test/0_initializeDatabase.js
+++ test/0_initializeDatabase.js
@@ -1,7 +1,10 @@
 import * as assert from 'node:assert';
+import fs from 'node:fs/promises';
 import { initializeCemeteryDatabase } from '../helpers/initializer.database.cemetery.js';
+import { lotOccupancyDB as databasePath } from '../data/databasePaths.js';
 describe('Initialize Database', () => {
     it('initializes a cemetery database', async () => {
+        await fs.unlink(databasePath);
         const success = await initializeCemeteryDatabase();
         assert.ok(success);
     });
--- test/0_initializeDatabase.ts
+++ test/0_initializeDatabase.ts
@@ -1,10 +1,16 @@
 /* eslint-disable unicorn/filename-case */
 
 import * as assert from 'node:assert'
+
+import fs from 'node:fs/promises'
+
 import { initializeCemeteryDatabase } from '../helpers/initializer.database.cemetery.js'
+import { lotOccupancyDB as databasePath } from '../data/databasePaths.js'
 
 describe('Initialize Database', () => {
   it('initializes a cemetery database', async () => {
+    await fs.unlink(databasePath)
+
     const success = await initializeCemeteryDatabase()
 
     assert.ok(success)
