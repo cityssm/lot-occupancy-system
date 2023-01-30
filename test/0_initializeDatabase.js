420314f5bcc554cd5d20cd396048cabb9d06c533
Dan Gowans <d.gowans@cityssm.on.ca>
2023-01-30 15:34:02 -0500

    avoid accidental database purges

--- test/0_initializeDatabase.js
+++ test/0_initializeDatabase.js
@@ -1,9 +1,12 @@
 import * as assert from 'node:assert';
 import fs from 'node:fs/promises';
 import { initializeCemeteryDatabase } from '../helpers/initializer.database.cemetery.js';
-import { lotOccupancyDB as databasePath } from '../data/databasePaths.js';
+import { lotOccupancyDB as databasePath, useTestDatabases } from '../data/databasePaths.js';
 describe('Initialize Database', () => {
     it('initializes a cemetery database', async () => {
+        if (!useTestDatabases) {
+            assert.fail('Test database must be used!');
+        }
         await fs.unlink(databasePath);
         const success = await initializeCemeteryDatabase();
         assert.ok(success);
