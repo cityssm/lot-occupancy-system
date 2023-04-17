88593e05e9e5337384c050776c2bc6d4895cc959
Dan Gowans <dangowans@users.noreply.github.com>
2023-04-17 13:23:10 -0400

    version bump

--- version.d.ts
+++ version.d.ts
@@ -1,2 +1,2 @@
-export declare const version = "1.0.0-alpha.12";
+export declare const version = "1.0.0-alpha.13";
 export default version;
--- version.ts
+++ version.ts
@@ -1,3 +1,3 @@
-export const version = '1.0.0-alpha.12'
+export const version = '1.0.0-alpha.13'
 
 export default version
