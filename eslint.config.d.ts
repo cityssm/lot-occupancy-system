47eb2f4abd2bc00082bcc78d715a0f9eddc6d1eb
Dan Gowans <dangowans@users.noreply.github.com>
2024-06-25 09:31:02 -0400

    fix eslint for client code

--- eslint.config.d.ts
+++ eslint.config.d.ts
@@ -1,1 +1,2 @@
-export { configWebApp as default } from 'eslint-config-cityssm';
+export declare const config: import("@typescript-eslint/utils/dist/ts-eslint").FlatConfig.ConfigArray;
+export default config;
--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,10 @@
-export { configWebApp as default } from 'eslint-config-cityssm';
+import { configWebApp } from 'eslint-config-cityssm';
+import tseslint from 'typescript-eslint';
+export const config = tseslint.config(...configWebApp, {
+    languageOptions: {
+        parserOptions: {
+            project: ['./tsconfig.json', './tsconfig.client.json']
+        }
+    }
+});
+export default config;
